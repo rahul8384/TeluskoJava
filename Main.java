ec0c200ddf23513f71fb5045d472710dc1efeb7a
rahul8384 <91208315+rahul8384@users.noreply.github.com>
2024-09-20 16:58:15 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,12 +1,12 @@
 package com.projects;
 
-class A extends Thread{
-    public void run(){
+class A extends Thread{ 
+    public void run(){ // Most important thing that in each thread based class method should be named run()
         for(int i=0; i<100; i++){
             System.out.println("hello");
         }
         try {
-            Thread.sleep(1);
+            Thread.sleep(1); // this is the method to put the thread in a state of wait for a certain time
         }catch(InterruptedException e){
             e.printStackTrace();
         }
@@ -28,10 +28,10 @@ class B extends Thread{
 public class Main {
 
     public static void main(String[] args) {
-        A obj1 = new A();
+        A obj1 = new A(); //creating the object of the class
         B obj2 = new B();
 
-        obj1.start();
+        obj1.start(); // start method is used to execute the method in each class
         obj2.start();
     }
 }
